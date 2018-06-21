e5084288ab331a882d536e9b1112557cbf959f82
System Administrator <root@Landons-MBP.attlocal.net>
2018-06-20 23:31:45 -0500

    Added node server basic setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 /* Random comments */
+const http = require('http');
+var fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('./main.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port);
+});
+
+/*
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
